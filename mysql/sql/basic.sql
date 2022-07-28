97512ce37d9c0cff3d6b18866c6b0b6722fc7b6b
kiloward96 <kiloward96@gmail.com>
2022-07-28 14:15:22 +0900

    commit

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -1,7 +1,12 @@
-use dolphin;
+USE dolphin;
 
-CREATE TABLE IF NOT EXISTS `dolphin`.`member2` (
-  `seq` INT NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`seq`))
-ENGINE = InnoDB
-;
+SELECT * FROM member2;
+
+ALTER TABLE member2 ADD COLUMN nickname VARCHAR(45);
+
+ALTER TABLE member2 ADD COLUMN nameEng VARCHAR(45) AFTER name;
+
+
+ALTER TABLE member2 MODIFY COLUMN nickname CHAR(90);
+
+ALTER TABLE member2 CHANGE COLUMN nickname starname varchar(45);
