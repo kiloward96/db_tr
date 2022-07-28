c25a8c6a2d33c3685893914bee4c5dbf15997231
kiloward96 <kiloward96@gmail.com>
2022-07-28 15:16:59 +0900

    db_last_commit

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -1,12 +1,35 @@
 USE dolphin;
 
-SELECT * FROM member2;
+SELECT * FROM dolphin.member2;
 
-ALTER TABLE member2 ADD COLUMN nickname VARCHAR(45);
+ALTER TABLE dolphin.member2 ADD COLUMN nickname VARCHAR(45);
 
-ALTER TABLE member2 ADD COLUMN nameEng VARCHAR(45) AFTER name;
+ALTER TABLE dolphin.member2 ADD COLUMN nameEng VARCHAR(45) AFTER name;
+
+-- 컬럼 데이터 속성변경
+
+ALTER TABLE dolphin.member2 MODIFY COLUMN nickname CHAR(90);
+
+-- 컬럼 명칭+데이터 속성 변경
+
+ALTER TABLE dolphin.member2 CHANGE COLUMN nickname starname varchar(45);
+
+-- 컬럼 삭제
+
+ALTER TABLE member2 DROP COLUMN starname;
+
+-- row 삭제
+
+SELECT seq, id, name FROM member;
+
+DELETE FROM member WHERE seq = 22;
+
+UPDATE member SET 
+id = 'test11' 
+WHERE
+seq = 21 
+AND id = 'KILOWARD96';
 
 
-ALTER TABLE member2 MODIFY COLUMN nickname CHAR(90);
 
-ALTER TABLE member2 CHANGE COLUMN nickname starname varchar(45);
+
