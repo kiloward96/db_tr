fb5974cc802b358755583b18f5db916b77be0623
kiloward96 <kiloward96@gmail.com>
2022-07-29 14:56:43 +0900

    DATA_COMMIT

--- mysql/sql/basic.sql
+++ mysql/sql/basic.sql
@@ -2,7 +2,7 @@ USE dolphin;
 
 SELECT * FROM dolphin.member2;
 
-ALTER TABLE dolphin.member2 ADD COLUMN nickname VARCHAR(45);
+ALTER TABLE dolphin.member ADD COLUMN nickname VARCHAR(45);
 
 ALTER TABLE dolphin.member2 ADD COLUMN nameEng VARCHAR(45) AFTER name;
 
@@ -32,4 +32,39 @@ AND id = 'KILOWARD96';
 
 
 
+SELECT @@character_set_database, @@collation_database;
 
+-- SET SESSION collation_database = utf8mb4_unicode_ci;
+
+SHOW VARIABLES LIKE 'collation%';
+
+ALTER TABLE `product_review` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+ALTER DATABASE dolphin CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+show variables like 'char%';
+
+select * from product;
+
+SELECT * FROM member
+WHERE 1=1
+-- AND name LIKE 'Lee%'
+-- AND name LIKE '%gwan'
+AND name LIKE '%Gyu%'
+;
+
+SELECT * FROM member
+WHERE 1=1
+-- AND gender = 0
+-- AND gender > 0
+AND gender >= 1
+-- AND gender BETWEEN 0 AND 1
+;
+
+SELECT * FROM member
+WHERE 1=1
+AND nicKname IS NULL
+-- AND nicKname IS NOT NULL
+-- LIMIT 3
+;
+UPDATE member SET nickname = '' WHERE seq = 21;
