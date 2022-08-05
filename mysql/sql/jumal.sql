963d5a25477e83e6ff575f27690f79f692ffe650
kiloward96 <kiloward96@gmail.com>
2022-08-05 09:03:45 +0900

    commit

--- mysql/sql/jumal.sql
+++ mysql/sql/jumal.sql
@@ -1,5 +1,16 @@
 USE dolphin;
 
+-- 공토코드불러오기
+select * from commonCodeGroup;
+select
+	a.seq as ccg
+    ,a.groupName as ccg
+    ,b.seq as cc
+    ,b.codeName as ccname
+    from commonCodeGroup a
+INNER JOIN commonCode b on b.commonCodeGroup_seq = a.seq order by a.seq, b.sort
+;
+
 -- 로그인
 
 SELECT
@@ -19,9 +30,15 @@ SELECT
     ,b.optionMain
     ,b.optionSub
 FROM product a
-	-- a = alias
 		 -- left join productOption b on b.product_seq = a.seq
 INNER JOIN productOption b on b.product_seq = a.seq
 ;
 
--- 
+-- 내 주문목록
+SELECT
+	a.seq
+    ,a.
+FROM memberChoiceOption a
+INNER JOIN 
+
+;
