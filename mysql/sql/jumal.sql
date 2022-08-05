c0fc16a4961e0e8d16dcd0e94b932c5d8ee547f6
kiloward96 <kiloward96@gmail.com>
2022-08-05 17:34:30 +0900

    commit

--- mysql/sql/jumal.sql
+++ mysql/sql/jumal.sql
@@ -3,18 +3,20 @@ USE dolphin;
 -- 공토코드불러오기
 select * from commonCodeGroup;
 select
-	a.seq as ccg
-    ,a.groupName as ccg
-    ,b.seq as cc
-    ,b.codeName as ccname
-    from commonCodeGroup a
-INNER JOIN commonCode b on b.commonCodeGroup_seq = a.seq order by a.seq, b.sort
+	ccg.seq as ccgSEQ
+    ,ccg.groupName as ccg
+    ,cc.sort as ccSEQ
+    ,cc.codeName as ccname
+    from commonCodeGroup ccg
+INNER JOIN commonCode cc on cc.commonCodeGroup_seq = ccg.seq order by ccg.seq, cc.sort
 ;
 
 -- 로그인
 
 SELECT
-	*
+	seq
+    ,id
+    ,password
 	FROM member 
 WHERE 1=1 
 AND id = 'kiloward96'
@@ -24,21 +26,21 @@ AND password = '1qaz2wsx1'
 -- 상품옵션목록
 
 SELECT
-	a.seq
-    ,a.productName
-    ,a.price
-    ,b.optionMain
-    ,b.optionSub
-FROM product a
-		 -- left join productOption b on b.product_seq = a.seq
-INNER JOIN productOption b on b.product_seq = a.seq
+	pd.seq
+    ,pd.productName
+    ,pd.price
+    ,pdop.optionMain
+    ,pdop.optionSub
+FROM product pd
+		 -- left join productOption pdop on pdop.product_seq = pd.seq
+INNER JOIN productOption pdop on pdop.product_seq = pd.seq
 ;
 
 -- 내 주문목록
 SELECT
-	a.seq
-    ,a.
-FROM memberChoiceOption a
+	mbco.seq
+    ,mbco.
+FROM memberChoiceOption mbco
 INNER JOIN 
 
 ;
