ecea6dc2adec97afd07b74fdae17714fb23cb3b2
kiloward96 <kiloward96@gmail.com>
2022-11-01 23:14:00 +0900

    commit

--- mysql/sql/index_view.sql
+++ mysql/sql/index_view.sql
@@ -1,6 +1,6 @@
 -- index 사용
-select * from member;
-CREATE INDEX mmIndex on member (delYn, grade, gender);
+SELECT * FROM member;
+CREATE INDEX mmIndex ON member (delYn, grade, gender);
 
 
 ALTER TABLE member DROP INDEX mmIndex;
@@ -11,4 +11,41 @@ CREATE VIEW memberV
 AS
 SELECT MB.seq, MB.name, MB.mobile, MA.addressZipcode, MA.addressMain, MA.addressDetail, MA.addressMore FROM member MB INNER JOIN memberAddress MA ON MB.seq = MA.member_seq WHERE 1=1 AND MB.delYn = 0 AND MA.defaultYn = 1;
 
+-- trigger
+CREATE DEFINER=`dbmaster`@`%` TRIGGER `member_BEFORE_INSERT` BEFORE INSERT ON `member` FOR EACH ROW BEGIN
+	INSERT INTO `dolphin.member`
+    (`seq`,
+    `name`,
+    `delYn`)
+    VALUES
+    ('99999',
+    '트리거_테스트',
+    '1');
+END;
 
+-- procedure
+
+-- function
+
+
+
+DELIMITER $$
+CREATE FUNCTION getMemberName (
+seq int
+) 
+RETURNS varchar(100)
+BEGIN
+	
+    declare rtName varchar(100);
+
+	select
+		name into rtName
+	from
+		member MB
+	where 1=1
+		and MB.seq = seq
+	;
+
+	RETURN rtName;
+END$$
+DELIMITER ;
